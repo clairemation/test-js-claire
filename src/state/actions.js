5616fea9dfa2a71a5dfdaaf6254e1b8ac11bd397
Claire Samuels <clairemation@gmail.com>
2017-04-21 21:49:24 -0400

    Implement getPopularMovies action

--- src/state/actions.js
+++ src/state/actions.js
@@ -3,18 +3,30 @@ import movies from './movies'
 
 
 export function getPopularMovies () {
-  //
-  // movies contains the results of two API requests
-  //
-
-  //
-  // 1. combine the results of these requests
-  // 2. sort the results FIRST by year THEN by title
-  // 3. each movie object in the results needs a releaseYear attribute added
-  //    this is used in src/components/movies-list.js line 25
-  //
-
-  const combinedResults = []
+  // Combine the two lists
+  var allMovies = movies[0].concat(movies[1]);
+  // Sift movies into year lists
+  var yearLists = {},
+    m = null,
+    year = null;
+  for (let i = 0; i < allMovies.length; i++){
+    m = allMovies[i];
+    year = (new Date(m.releaseDate)).getFullYear();
+    m.releaseYear = year;
+    yearLists[year] = yearLists[year] ? yearLists[year].concat(m) : [m] ;
+  }
+  // Put the years in order
+  var orderedYearLists = Object.keys(yearLists).sort().map(year => yearLists[year]);
+  // Sort each year's movie list by title
+  orderedYearLists = orderedYearLists.map(list =>
+    list.sort((a, b) => {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    })
+  );
+  // Flatten the array of lists
+  const combinedResults = orderedYearLists.reduce((acc, curr) => acc.concat(curr) );
 
   return {
     type: 'GET_MOVIES_SUCCESS',
